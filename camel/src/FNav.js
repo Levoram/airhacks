2e42a94421344160b33f3a1b0eaa05094251e5f7
Adam Bien <abien@adam-bien.com>
2018-11-07 13:26:16 +0100

    fetch request-response

--- camel/src/FNav.js
+++ camel/src/FNav.js
@@ -1,10 +1,22 @@
-export default class FNav extends HTMLElement{ 
+export default class FNav extends HTMLElement {
 
 
-    connectedCallback(){ 
+    connectedCallback() {
+        this.loadFromServer();
+    }
+
+    display(serverobject) { 
+        const { message } = serverobject;
         this.innerHTML = `
-            <h3>hey joe</h3>
+            <h3>${message}</h3>
         `;
+    }
+
+
+    loadFromServer() { 
+        fetch('gates.json').
+            then(r => r.json()).
+            then(json => this.display(json));
     }
 }
 
