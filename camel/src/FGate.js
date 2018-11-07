52e1fad3dafe0a8b74ad12dc62bd0756dbb00285
Adam Bien <abien@adam-bien.com>
2018-11-07 16:07:12 +0100

    xss implemented

--- camel/src/FGate.js
+++ camel/src/FGate.js
@@ -1,7 +1,26 @@
+const basics = (first, second) => { 
+    console.log(first, second);
+    return "heute nicht";
+}
 export default class FGate extends HTMLElement { 
 
+    constructor() { 
+        super();
+        this.root = this.attachShadow({'mode':'open'});
+    }
+
     connectedCallback() { 
-        this.innerText = "what a nice gate";
+        const age = `<img src="not exist" onerror="console.log('hey joe')"/>`;
+        this.root.innerHTML = `  
+        <style>
+        h2{
+            font-size: 2em;
+        }
+
+        </style>
+        
+        <h2> what a nice gate ${age}</h2 >
+            `;
     }
 
 }
